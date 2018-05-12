33dc801ff5b7f9b210e044539b6e1e5868a5f1f7
Mohitdeep Singh <msingh@pandora.com>
2018-05-11 23:33:30 -0400

    first commit

---
+++ Variables, Datatypes.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 33
+print(myAge)
+
+let myName : String = "Mohit"
+let mySurName : String = "Singh"
+let myAgeIntTenYears = myAge + 10
+let myFullName : String = "\(myName) \(mySurName)"
+
+let myDetails : String = "\(myFullName), \(myAge)";
+
+let booleans : Bool = true
+let floatPointNumber : Float = 1.3;
+
+let doublePointNumber : Double = 1.2324343;
