870991ec06de4d576440b1b2a381588401d1b3d2
Mohitdeep Singh <msingh@pandora.com>
2018-05-13 15:15:52 -0400

    loops

--- Comments, Printing and console.playground/Contents.swift
+++ Comments, Printing and console.playground/Contents.swift
@@ -34,11 +34,72 @@ func loveCalculator(yourName: String, theirName: String) -> String{
     if score > 80{
         return "You love each other"
     }
+        
+    else if score > 40 && score <= 80{
+        return "some spark is there.. not a lot.. some"
+    }
     else{
         return "you dont love each other"
     }
 }
 
 loveCalculator(yourName: "foo", theirName: "bar")
+
+
+func bmiCalculator(mass: Int, height: Int) -> Double{
+    if height == 0 {
+        return 0.0
+    }
+    let bmi = Double(mass) / pow(Double(height), 2.0)
+    return bmi
+}
+
+var bmi = bmiCalculator(mass: 30, height: 2)
+
+
+
+let arrayOfNumbers = [1,2,3,4,5]
+
+var sum = 0
+
+for number in arrayOfNumbers{
+    sum += number
+}
+print(sum)
+
+for number in 1...10{
+    print(number)
+}
+
+for number in 1...10 where number % 2 == 0{
+    print(number)
+}
+
+
+func fibonacciNumbers(until n: Int) -> Int {
+    if n == 0 {
+        return 0
+    }
+    else if n == 1{
+        return 1
+    }
+    var secondLast = 0;
+    var last = 1
+    var this = 0
+    for number in 2...n {
+        this = last + secondLast
+        secondLast = last
+        last = this
+        
+    }
+    
+    return this
+    
+}
+
+
+var fib = fibonacciNumbers(until: 6)
+print(fib)
+
 
 
