07721f39a7b249a8a75994c9c8702da240999b62
Mohit <mohit1007@gmail.com>
2018-05-29 01:54:12 -0400

    todo app

---
+++ Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by msingh on 5/29/18.
+//  Copyright © 2018 rockitman. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
